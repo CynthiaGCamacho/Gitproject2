f912cff7e5773232091a6c423d7a109327c9a41e
cynthia <cynthia@LAPTOP-G4CRUIVD.fios-router.home>
2023-07-29 11:46:35 -0400

    created a new line

--- src/Packageday2/DemoClasstest2.java
+++ src/Packageday2/DemoClasstest2.java
@@ -4,7 +4,7 @@ public class DemoClasstest2 {
 	
 	public static void main(String []args) {
 		System.out.println("Zoom class");
-		System.out.printl("Cynthia");
+		System.out.println("Cynthia");
 	}
 
 }
